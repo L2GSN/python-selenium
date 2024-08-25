ab4080320bc00a9cba7880a09c819499c0a4393c
dylan-Macmini <1435385838@qq.com>
2024-08-25 13:06:36 +0800

    finish stock page operation

--- stock-auto.py
+++ stock-auto.py
@@ -4,10 +4,19 @@ from selenium.webdriver.common.by import By
 
 # create Chrome webdriver object
 wd = webdriver.Chrome()
+wd.implicitly_wait(5)
 
 # open target website by Get method
 wd.get('https://www.byhy.net/_files/stock1.html')
 
-# select element by using id
+# select element by using id='kw'
 element = wd.find_element(By.ID, 'kw')
 
+# input content into selected element
+element.send_keys('通讯\n')
+elements = wd.find_elements(By.ID, '1')
+
+for element in elements:
+    print(element.text)
+
+pass
