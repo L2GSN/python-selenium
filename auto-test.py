e7bb8dc72b59c402c08084cb033a5e2b59666008
dylan-Macmini <1435385838@qq.com>
2024-08-28 22:33:40 +0800

    remove functions.py, completed the assertion of UI-0001~UI0005

--- auto-test.py
+++ auto-test.py
@@ -8,24 +8,24 @@ wd.implicitly_wait(5)
 wd.get('http://127.0.0.1')
 
 # UI-0001
-# # Find the password input frame
-# element = wd.find_element(By.CSS_SELECTOR, 'input#password')
-#
-#
-# # input password=88888888
-# element.send_keys('88888888')
-#
-# # Find the '登陆' button and click
-# wd.find_element(By.TAG_NAME, 'button').click()
-#
-# # Judge if the alert text is '请输入用户名'
-# alert_text = '请输入用户名'
-# if wd.switch_to.alert.text == alert_text:
-#     wd.switch_to.alert.accept()
-#     print('UI-0001: PASS')
-#
-# else:
-#     print(wd.switch_to.alert.text)
+# Find the password input frame
+element = wd.find_element(By.CSS_SELECTOR, 'input#password')
+
+
+# input password=88888888
+element.send_keys('88888888')
+
+# Find the '登陆' button and click
+wd.find_element(By.TAG_NAME, 'button').click()
+
+# Judge if the alert text is '请输入用户名'
+alert_text = '请输入用户名'
+if wd.switch_to.alert.text == alert_text:
+    wd.switch_to.alert.accept()
+    print('UI-0001: PASS')
+
+else:
+    print(wd.switch_to.alert.text)
 
 # UI-0002
 # Find 'username' input box and clear content
@@ -40,8 +40,9 @@ wd.find_element(By.CSS_SELECTOR, 'input#password').clear()
 # Click '登陆‘ button
 wd.find_element(By.TAG_NAME, 'button').click()
 
-# Judge if the alert text is '请输入密码'
+# Assert if the alert text is '请输入密码'
 alert_text = '请输入密码'
+
 if wd.switch_to.alert.text == alert_text:
     wd.switch_to.alert.accept()
     print('UI-0002: PASS')
@@ -50,46 +51,76 @@ else:
     print(wd.switch_to.alert.text)
 
 # UI-0003
-# # Find 'username' input box and input 'byh'
-# element = wd.find_element(By.CSS_SELECTOR,'input#username')
-# element.clear()
-# element.send_keys('byh')
-#
-# # Find the password input box
-# element = wd.find_element(By.CSS_SELECTOR, 'input#password')
-# element.clear()
-# element.send_keys('88888888')
-#
-# # Click '登陆‘ button
-# wd.find_element(By.TAG_NAME, 'button').click()
+# Find 'username' input box and input 'byh'
+element = wd.find_element(By.CSS_SELECTOR, 'input#username')
+element.clear()
+element.send_keys('byh')
+
+# Find the password input box
+element = wd.find_element(By.CSS_SELECTOR, 'input#password')
+element.clear()
+element.send_keys('88888888')
+
+# Click '登陆‘ button
+wd.find_element(By.TAG_NAME, 'button').click()
+
+# Assert if the alert text is '登录失败 : 用户名或者密码错误'
+alert_text = '登录失败 : 用户名或者密码错误'
+
+if wd.switch_to.alert.text == alert_text:
+    wd.switch_to.alert.accept()
+    print('UI-0003: PASS')
+
+else:
+    print(wd.switch_to.alert.text)
 
 # UI-0004
-# # Find 'username' input box and input 'byh'
-# element = wd.find_element(By.CSS_SELECTOR,'input#username')
-# element.clear()
-# element.send_keys('byhy')
-#
-# # Find the password input box
-# element = wd.find_element(By.CSS_SELECTOR, 'input#password')
-# element.clear()
-# element.send_keys('8888888')
-#
-# # Click '登陆‘ button
-# wd.find_element(By.TAG_NAME, 'button').click()
+# Find 'username' input box and input 'byh'
+element = wd.find_element(By.CSS_SELECTOR,'input#username')
+element.clear()
+element.send_keys('byhy')
+
+# Find the password input box
+element = wd.find_element(By.CSS_SELECTOR, 'input#password')
+element.clear()
+element.send_keys('8888888')
+
+# Click '登陆‘ button
+wd.find_element(By.TAG_NAME, 'button').click()
+
+# Assert if the alert text is '登录失败 : 用户名或者密码错误'
+alert_text = '登录失败 : 用户名或者密码错误'
+
+if wd.switch_to.alert.text == alert_text:
+    wd.switch_to.alert.accept()
+    print('UI-0004: PASS')
+
+else:
+    print(wd.switch_to.alert.text)
 
 # UI-0005
-# # Find 'username' input box and input 'byh'
-# element = wd.find_element(By.CSS_SELECTOR,'input#username')
-# element.clear()
-# element.send_keys('byhy')
-#
-# # Find the password input box
-# element = wd.find_element(By.CSS_SELECTOR, 'input#password')
-# element.clear()
-# element.send_keys('888888888')
-#
-# # Click '登陆‘ button
-# wd.find_element(By.TAG_NAME, 'button').click()
+# Find 'username' input box and input 'byh'
+element = wd.find_element(By.CSS_SELECTOR,'input#username')
+element.clear()
+element.send_keys('byhy')
+
+# Find the password input box
+element = wd.find_element(By.CSS_SELECTOR, 'input#password')
+element.clear()
+element.send_keys('888888888')
+
+# Click '登陆‘ button
+wd.find_element(By.TAG_NAME, 'button').click()
+
+# Assert if the alert text is '登录失败 : 用户名或者密码错误'
+alert_text = '登录失败 : 用户名或者密码错误'
+
+if wd.switch_to.alert.text == alert_text:
+    wd.switch_to.alert.accept()
+    print('UI-0005: PASS')
+
+else:
+    print(wd.switch_to.alert.text)
 
 # UI-0101
 # # Find 'username' input box and input 'byh'
